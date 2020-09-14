21cf9baab711702d8b8d69204482341970614006
Anton Veldanov <anton.veldanov@gmail.com>
2020-09-14 13:19:42 -0700

    iOS

---
+++ FabNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NewsListTableViewController.swift
+//  FabNews
+//
+//  Created by Veldanov, Anton on 9/14/20.
+//
+
+import UIKit
+
+class NewsListTableViewController: UITableViewController{
+    
+    
+    
+    
+}
