712604fb850841f9a5f2e6dc3687b952aa51fa94
Anton Veldanov <anton.veldanov@gmail.com>
2020-09-16 12:18:47 -0700

    iOS

---
+++ FabNews/Models/Article.swift
@@ -0,0 +1,26 @@
+//
+//  Article.swift
+//  FabNews
+//
+//  Created by Veldanov, Anton on 9/14/20.
+//
+
+import Foundation
+
+struct ArticleList: Decodable{
+    var articles: [Article]
+}
+
+//struct Status: Decodable {
+//    let status: String
+//}
+
+
+struct Article: Decodable {
+    let title: String
+    let description: String?
+}
+
+//struct Article: Decodable {
+//    let status: String
+//}
---
+++ FabNews/ViewModels/ArticleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ArticleViewModel.swift
+//  FabNews
+//
+//  Created by Veldanov, Anton on 9/14/20.
+//
+
+import Foundation
